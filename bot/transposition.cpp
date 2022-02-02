ac37caa18c83862db201b6b6a691de8aae004bff
citrus610 <79373642+citrus610@users.noreply.github.com>
2022-02-02 20:44:00 +0700

    fix grammar, embarrassing...

--- bot/transposition.cpp
+++ bot/transposition.cpp
@@ -1,35 +1,35 @@
-#include "tranposition.h"
+#include "transposition.h"
 
 namespace LemonTea
 {
 
-TranpositionTable::TranpositionTable()
+TranspositionTable::TranspositionTable()
 {
     this->size = 0;
     this->bucket = nullptr;
 };
 
-TranpositionTable::~TranpositionTable()
+TranspositionTable::~TranspositionTable()
 {
     if (this->bucket != nullptr) {
         this->destroy();
     }
 };
 
-uint32_t TranpositionTable::hash(Board& board)
+uint32_t TranspositionTable::hash(Board& board)
 {
     return murmur_hash_3_x86_32((const uint8_t*)board.data, 80, 0b1010101010101010);
 };
 
-void TranpositionTable::init(int power)
+void TranspositionTable::init(int power)
 {
 
     this->size = 1 << power;
-    this->bucket = (TranpositionBucket*)malloc(this->size * sizeof(TranpositionBucket));
+    this->bucket = (TranspositionBucket*)malloc(this->size * sizeof(TranspositionBucket));
     this->clear();
 };
 
-void TranpositionTable::destroy()
+void TranspositionTable::destroy()
 {
     if (this->bucket == nullptr) {
         return;
@@ -38,22 +38,22 @@ void TranpositionTable::destroy()
     this->bucket = nullptr;
 };
 
-void TranpositionTable::clear()
+void TranspositionTable::clear()
 {
     if (this->bucket == nullptr) {
         return;
     }
-    memset(this->bucket, 0, sizeof(TranpositionBucket) * this->size);
+    memset(this->bucket, 0, sizeof(TranspositionBucket) * this->size);
 };
 
 // Add an entry to the table
 // Return true if add successfully
-bool TranpositionTable::add(uint32_t hash, int attack)
+bool TranspositionTable::add(uint32_t hash, int attack)
 {
-    TranpositionEntry* slot = this->bucket[hash & (this->size - 1)].slot;
+    TranspositionEntry* slot = this->bucket[hash & (this->size - 1)].slot;
 
     // If there is matching hash key, replace if new attack is better
-    for (int i = 0; i < TRANPOSITION_BUCKET_SIZE; ++i) {
+    for (int i = 0; i < TRANSPOSITION_BUCKET_SIZE; ++i) {
         if (slot[i].key == hash) {
             if (slot[i].attack < attack) {
                 slot[i].attack = attack;
@@ -64,7 +64,7 @@ bool TranpositionTable::add(uint32_t hash, int attack)
     }
 
     // Else, if there is an empty slot in the bucket, just fill in
-    for (int i = 0; i < TRANPOSITION_BUCKET_SIZE; ++i) {
+    for (int i = 0; i < TRANSPOSITION_BUCKET_SIZE; ++i) {
         if (slot[i].key == 0 && slot[i].attack == 0) {
             slot[i].key = hash;
             slot[i].attack = attack;
@@ -74,7 +74,7 @@ bool TranpositionTable::add(uint32_t hash, int attack)
 
     // Else, try to replace the slot with smallest attack eval
     int smallest_attack_index = 0;
-    for (int i = 1; i < TRANPOSITION_BUCKET_SIZE; ++i) {
+    for (int i = 1; i < TRANSPOSITION_BUCKET_SIZE; ++i) {
         if (slot[i].attack < slot[smallest_attack_index].attack) {
             smallest_attack_index = i;
         }
@@ -88,12 +88,12 @@ bool TranpositionTable::add(uint32_t hash, int attack)
     return false;
 };
 
-// Get the tranposition entry
+// Get the Transposition entry
 // Return true if found matching key
-bool TranpositionTable::get(uint32_t hash, int& attack)
+bool TranspositionTable::get(uint32_t hash, int& attack)
 {
-    TranpositionEntry* slot = this->bucket[hash & (this->size - 1)].slot;
-    for (int i = 0; i < TRANPOSITION_BUCKET_SIZE; ++i) {
+    TranspositionEntry* slot = this->bucket[hash & (this->size - 1)].slot;
+    for (int i = 0; i < TRANSPOSITION_BUCKET_SIZE; ++i) {
         if (slot[i].key == hash) {
             attack = slot[i].attack;
             return true;
@@ -104,17 +104,17 @@ bool TranpositionTable::get(uint32_t hash, int& attack)
 
 // Take a sample of the first 1000 buckets in the table
 // Estimate the permill of used buckets in hash table
-int TranpositionTable::hashfull()
+int TranspositionTable::hashfull()
 {
     int used = 0;
     for (uint32_t i = 0; i < std::min(uint32_t(1000), this->size); ++i) {
-        for (int k = 0; k < TRANPOSITION_BUCKET_SIZE; ++k) {
+        for (int k = 0; k < TRANSPOSITION_BUCKET_SIZE; ++k) {
             if (this->bucket[i].slot[k].key != 0) {
                 used += 1;
             }
         }
     }
-    return used / TRANPOSITION_BUCKET_SIZE;
+    return used / TRANSPOSITION_BUCKET_SIZE;
 };
 
 };
